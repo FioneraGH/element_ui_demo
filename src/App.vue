9472cb6f716888c1ef3368d2122c6ae5a5a8b699
FioneraGH <fionera@outlook.com>
2017-01-10 21:59:42 +0800

    init repo

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <el-button @click.native="startHacking">通知</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: '欢迎使用'
+      }
+    },
+    methods: {
+      startHacking() {
+        this.$notify({
+          title: '通知',
+          message: '运行成功',
+          duration: 2500
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
